f4fba3656f510fa534d84984ce21076bffe07538
Lusiana lSuwan. Suwandi <lSuwandi@esrisa.co.id>
2017-11-24 00:28:56 +0700

    del

--- streetview/widgets/Mapillary/nls/strings.js
+++
@@ -1,9 +0,0 @@
-/* globals define */
-define({
-  root: {
-    widgetTitle: 'Mapillary',
-    description: 'Viewer using MapillaryJS to visualize Mapillary images'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-})
